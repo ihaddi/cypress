a57e40131af82f1134a7d7f4f88c7c379789446e
Hadi Mousavi <me@haddi.ir>
2020-04-20 03:10:03 +0430

    add config file and babel file then create start

---
+++ config/webpack.dev.js
@@ -0,0 +1,98 @@
+const webpack = require("webpack");
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const rootFolder = path.resolve(__dirname, "..");
+const SrcFolder = rootFolder + "/src";
+const SrcAssets = SrcFolder + "/assets";
+const distFolder = rootFolder + "/dist";
+const distAssetFolder = distFolder + "/assets";
+
+module.exports = {
+  entry: {
+    vendor: rootFolder + "/index.js",
+  },
+  mode: "development",
+  module: {
+      
+    rules: [
+      // Add Babel loader
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        resolve: {
+            extensions: [".js", ".jsx"]
+          },
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      
+
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+
+              // // use for separate css pages (custom pages, eg. wizard, invoices, etc.)
+              // includePaths: demos.map((demo) => {
+              //     return slash(srcPath) + "/sass/theme/demos/" + demo;
+              // })
+            },
+          },
+        ],
+      },
+
+      //Images Loader
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: "file-loader",
+        options: {
+          outputPath: "images",
+          publicPath: "assets",
+        },
+      },
+      //   html loader
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+
+     
+    ],
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: SrcFolder + "/index.html",
+    }),
+    // create css file
+    new MiniCssExtractPlugin({
+        filename: distAssetFolder + "css/style.[name].css",
+      }),
+  ],
+
+  output: {
+    filename: "bundle.js",
+    path: distFolder,
+  },
+  devServer: {
+    contentBase: distFolder,
+    compress: true,
+    hot: true,
+    port: 9000,
+  },
+};
---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import '../scss/style.scss';
+
+
+class App extends Component {
+   
+    render() { 
+        return ( 
+            <div>
+                <h2>Welcome to2 Cypress</h2>
+            </div>
+         );
+    }
+}
+ 
+export default App;
